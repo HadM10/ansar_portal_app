b48d46a8ff11da4961a7e0674722f8cba5d08575
HadM10 <hadmak10@gmail.com>
2024-05-25 14:27:28 +0000

    26th commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'home.dart';
 import 'sign_in.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -14,30 +13,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<String?>(
-      future: storage.read(key: 'isSignedIn'),
-      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-        if (snapshot.hasData && snapshot.data == 'true') {
-          return MaterialApp(
-            title: 'Ansar Portal',
-            theme: ThemeData(
-              primarySwatch: Colors.deepOrange,
-            ),
-            home: const HomePage(), // Navigate to home page if signed in
-            debugShowCheckedModeBanner: false,
-          );
-        } else {
-          return MaterialApp(
-            title: 'Ansar Portal',
-            theme: ThemeData(
-              primarySwatch: Colors.deepOrange,
-            ),
-            home: const SignInPage(),
-            // Navigate to sign-in page if not signed in
-            debugShowCheckedModeBanner: false,
-          );
-        }
-      },
+    return MaterialApp(
+      title: 'Ansar Portal',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        // Set the background color for the default white screen
+        backgroundColor: Colors.deepOrange,
+      ),
+      home: FutureBuilder<String?>(
+        future: storage.read(key: 'isSignedIn'),
+        builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+          if (snapshot.hasData && snapshot.data == 'true') {
+            return const HomePage(); // Navigate to home page if signed in
+          } else {
+            return const SignInPage(); // Navigate to sign-in page if not signed in
+          }
+        },
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
